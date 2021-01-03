7aec3af6ba6318ebd3d71775ed73f00b485ff573
DMinghao <minghaodu96@gmail.com>
2021-01-02 18:28:18 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'react-bootstrap/lib/Button';
-import FormControl from 'react-bootstrap/lib/FormControl';
-import InputGroup from 'react-bootstrap/lib/InputGroup';
-import Overlay from 'react-bootstrap/lib/Overlay';
-import Popover from 'react-bootstrap/lib/Popover';
+import {Button, FormControl, InputGroup, Overlay, Popover} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
 
